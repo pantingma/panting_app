976a5af4ee1fda03b478e0924145a16b308b5d58
Shaoxiao Xu <shaoxixu@tesla.com>
2023-03-03 15:40:28 +0800

    first commit

---
+++ lib/pages/cv_panting.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CVPanting extends StatelessWidget {
+
+  final Widget title;
+
+  const CVPanting({required this.title, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
